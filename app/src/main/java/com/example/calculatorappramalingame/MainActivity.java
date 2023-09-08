f76a8b7eb9ca7b997a34cf8f306a98bc5e1afe34
rriya625 <riya.ramalingame1603@gmail.com>
2023-09-08 10:41:10 -0500

    Some design, Added all extra activities, on click button which activity to open

--- app/src/main/java/com/example/calculatorappramalingame/MainActivity.java
+++ app/src/main/java/com/example/calculatorappramalingame/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.calculatorappramalingame;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void switchScreens(View view){
+
+        if(view.getId() == R.id.additionButton){
+            Intent intent = new Intent(this, AdditionActivity.class);
+            startActivity(intent);
+        }
+        else if (view.getId() == R.id.subtractionButton){
+            Intent intent = new Intent(this, SubtractionActivity.class);
+            startActivity(intent);
+        }
+        else if(view.getId() == R.id.multiplicationButton){
+            Intent intent = new Intent(this, MultiplicationActivity.class);
+            startActivity(intent);
+        }
+        else if(view.getId() == R.id.divisionButton){
+            Intent intent = new Intent(this, DivisionActivity.class);
+            startActivity(intent);
+        }
+        else if(view.getId() == R.id.exponentialButton){
+            Intent intent = new Intent(this, ExponentialActivity.class);
+            startActivity(intent);
+        }
+        else if(view.getId() == R.id.integerDivisionButton){
+            Intent intent = new Intent(this, IntegerDivisionActivity.class);
+            startActivity(intent);
+        }
     }
 }
 
